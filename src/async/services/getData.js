5d95f2f64b0d90c06b1e65cc4a0bcb256e7d5b6e
Angel <aburgoapaco@gmail.com>
2024-04-30 13:52:39 -0400

    subiendo modificacion

--- src/async/services/getData.js
+++ src/async/services/getData.js
@@ -1,9 +1,10 @@
 ( async () => {
 
-  const response = await fetch('https://dummyjson.com/products/1');
-  
-  const data = await response.json();
+  const ID = Math.ceil(Math.random() * 100);
 
+  const response = await fetch(`https://dummyjson.com/products/${ID}`);
+  const data = await response.json();
   console.log(data);
+  
 
 })();
